a30790b99adb7ab2bf41f32876cb181777f7198a
Berar-Denisa-Maria <denisa.berar@stud.ubbcluj.ro>
2024-12-15 20:32:30 +0200

    aproape gata+eroare cand dau show on maps

---
+++ Models/Shop.cs
@@ -0,0 +1,21 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLiteNetExtensions.Attributes;
+namespace BerarDenisaLab7.Models
+{
+    public class Shop
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; } 
+        public string ShopName { get; set; } = string.Empty;
+        public string Adress { get; set; } = string.Empty;
+        public string ShopDetails => $"{ShopName}, {Adress}";
+
+        [OneToMany]
+        public List<ShopList> ShopLists { get; set; } = new();
+    }
+}
--- Models/ShopList.cs
+++ Models/ShopList.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SQLite;
+using SQLiteNetExtensions.Attributes;
 namespace BerarDenisaLab7.Models
 {
     public class ShopList
@@ -11,8 +12,11 @@ namespace BerarDenisaLab7.Models
         [PrimaryKey, AutoIncrement]
         public int ID { get; set; }
         [MaxLength(250), Unique]
-        public string Description { get; set; }
+        public string Description { get; set; } 
         public DateTime Date { get; set; }
+
+        [ForeignKey(typeof(Shop))]
+        public int ShopID { get; set; }
 
     }
 }
